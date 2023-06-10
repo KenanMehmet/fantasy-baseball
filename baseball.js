dcc0f51ccb875b2e81137280e914224522a990a8
Kenan Mehmet <kenanmehmet@Kenans-MacBook-Pro.local>
2023-06-10 17:45:34 +0100

    first commit

---
+++ baseball.js
@@ -0,0 +1,16 @@
+class Players {
+    name;
+    age;
+    batting;
+    pitching;
+    catching;
+    position;
+    rightHanded;
+}
+
+
+let team_one = [];
+let team_two = [];
+let bases = [];
+let strikeOuts = 0;
+let innings;
